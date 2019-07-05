4b6e5d4c578af8f996944576e7fe0c462c026e12
Léa Tanda <leaztanda@gmail.com>
2019-07-05 16:19:24 +0200

    :label: add types

---
+++ client/src/@types/index.d.ts
@@ -0,0 +1,19 @@
+export interface UserI {
+  id: string,
+  createdAt: number,
+  name: string
+}
+
+export interface MessageI {
+  createdAt: number,
+  from: UserI,
+  content: string
+}
+
+export interface RoomI {
+  id: string,
+  createdAt: number,
+  host: UserI,
+  name: string,
+  users: UserI[]
+}
