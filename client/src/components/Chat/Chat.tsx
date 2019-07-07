99a31b13a748106747faf076fadcecd28b3e99f1
itsleaht <leaztanda@gmail.com>
2019-07-07 22:32:23 +0200

    :sparkles: :lipstick: add notification system

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -15,13 +15,27 @@ type User = {
 const Chat: FunctionComponent = () => {
   const [user, setUser] = useState<User>({id: ''});
   const [isMinified, setisMinified] = useState<boolean>(true);
+  const [counterNotification, setCounterNotification] = useState<number>(0);
+  const maxCounterNotification = 99
 
   const onClickToggle = () => {
     setisMinified(!isMinified);
+
+    const timeout = setTimeout(() => {
+      clearTimeout(timeout);
+      setCounterNotification(0);
+
+    }, 350)
   }
 
   useSocketOn('user:info', userInfo => {
     setUser(userInfo)
+  });
+
+  useSocketOn('chat:message', userInfo => {
+    if (isMinified && counterNotification <= maxCounterNotification) {
+      setCounterNotification(counterNotification + 1);
+    }
   });
 
   return (
@@ -31,7 +45,7 @@ const Chat: FunctionComponent = () => {
           <div className="chat__header__left">
             <div className="chat__notification">
               <Icon extraClasses="chat__notification__icon" name="message-notification" fill="#4A5CFF" width={40} height={27}  />
-              <span className="chat__notification__text"></span>
+              <span className="chat__notification__text">{counterNotification}{counterNotification >=  maxCounterNotification ? '+' : ''}</span>
             </div>
             <span className="heading-3 chat__title">The artist team</span>
           </div>
