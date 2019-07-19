7c08435a40a983c85657c303a93ddc0314a85fc5
Léa Tanda <leaztanda@gmail.com>
2019-07-19 14:53:17 +0200

    :construction: start toolbox panel

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
-import logo from './logo.svg';
 
 import './_header.styl';
+import ToolboxPanel from "../ToolboxPanel/ToolboxPanel";
 
 const Header: FunctionComponent = () => {
   return (
@@ -12,7 +12,9 @@ const Header: FunctionComponent = () => {
             <img src={`${process.env.PUBLIC_URL}/assets/logo.svg`} className="header__logo__image" alt="logo" />
           </a>
         </div>
-        <div className="header__middle"></div>
+        <div className="header__middle">
+          <ToolboxPanel />
+        </div>
         <div className="header__right"></div>
       </div>
     </header>
---
+++ client/src/components/ToolboxPanel/PenToolbox/PenToolbox.tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponent, useState } from "react";
+
+import './_toolbox-pen.styl';
+
+const PenToolbox: FunctionComponent = () => {
+  const [activePen, setActivePen] = useState<string>('sm');
+  const sizes = ['sm', 'md', 'lg'];
+
+  const onClickBtnPen = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const currentItem = e.currentTarget.getAttribute('data-index');
+    if (currentItem) {
+      setActivePen(currentItem);
+    }
+  }
+
+  return(
+    <div className="toolbox toolbox--pen">
+      <span>Pinceau</span>
+      {sizes.map(item => {
+        return (
+          <button className={`toolbox__button toolbox__button--${item} ${activePen === item ? 'is-active' : ''}`} onClick={onClickBtnPen} data-index={item}></button>
+        )
+      })}
+    </div>
+  )
+}
+
+export default PenToolbox;
